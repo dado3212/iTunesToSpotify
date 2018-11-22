a4baa35fce0c84ffaba343df8386e42543849c4b
Alex Beals <abeals@fb.com>
2018-11-22 13:29:34 -0800

    Initial Commit

---
+++ iTunesToSpotify/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iTunesToSpotify
+//
+//  Created by Alex Beals on 11/22/18.
+//  Copyright © 2018 Alex Beals. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
